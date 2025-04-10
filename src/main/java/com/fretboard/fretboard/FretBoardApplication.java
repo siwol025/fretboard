9bfa3b1336d712452b38fbd0cf7a2ebe9418115c
siwol025 <cyk99115695@gmail.com>
2025-04-10 14:56:53 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/fretboard/fretboard/FretBoardApplication.java
@@ -0,0 +1,13 @@
+package com.fretboard.fretboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FretBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FretBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fretboard/fretboard/FretBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fretboard.fretboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FretBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
