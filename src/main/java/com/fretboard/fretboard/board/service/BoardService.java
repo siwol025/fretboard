1b82eae8d0333cc9df17951ca1448ec679490024
siwol025 <cyk99115695@gmail.com>
2025-04-14 15:23:52 +0900

    feat: 게시판 제목 수정 기능 구현

--- src/main/java/com/fretboard/fretboard/board/service/BoardService.java
+++ src/main/java/com/fretboard/fretboard/board/service/BoardService.java
@@ -3,6 +3,8 @@ package com.fretboard.fretboard.board.service;
 import com.fretboard.fretboard.board.domain.Board;
 import com.fretboard.fretboard.board.dto.BoardRequest;
 import com.fretboard.fretboard.board.repository.BoardRepository;
+import com.fretboard.fretboard.exception.ExceptionType;
+import com.fretboard.fretboard.exception.FretBoardException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,5 +19,12 @@ public class BoardService {
     public Long addBoard(final BoardRequest boardRequest) {
         Board saveBoard = boardRepository.save(boardRequest.toBoard());
         return saveBoard.getId();
+    }
+
+    @Transactional
+    public void editBoardTitle(final Long id, final BoardRequest boardRequest) {
+        Board board = boardRepository.findById(id)
+                .orElseThrow(() -> new FretBoardException(ExceptionType.BOARD_NOT_FOUNT));
+        board.setTitle(boardRequest.title());
     }
 }
--- src/main/java/com/fretboard/fretboard/exception/ExceptionType.java
+++ src/main/java/com/fretboard/fretboard/exception/ExceptionType.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ExceptionType {
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 게시물을 찾을 수 없습니다."),
+    BOARD_NOT_FOUNT(HttpStatus.NOT_FOUND, "해당 게시판을 찾을 수 없습니다."),
     DUPLICATION_USERNAME(HttpStatus.BAD_REQUEST, "중복된 아이디입니다."),
     DUPLICATION_NICKNAME(HttpStatus.BAD_REQUEST, "중복된 닉네임입니다.");
 
