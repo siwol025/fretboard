f849b1b1b7872c6a96f0084a6735ed14e00d8ed1
siwol025 <cyk99115695@gmail.com>
2025-04-15 20:48:58 +0900

    feat: 댓글 수정기능 구현

--- src/main/java/com/fretboard/fretboard/comment/controller/CommentController.java
+++ src/main/java/com/fretboard/fretboard/comment/controller/CommentController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +30,12 @@ public class CommentController {
     public ResponseEntity<CommentResponse> findComments(@PathVariable Long postId) {
         CommentResponse comments = commentService.findComments(postId);
         return ResponseEntity.ok().body(comments);
+    }
+
+    @PutMapping("/comments/{id}")
+    public ResponseEntity<Void> editComment(@PathVariable Long id,
+                                            @Valid @RequestBody CommentRequest commentRequest) {
+        commentService.editComment(id, commentRequest);
+        return ResponseEntity.noContent().build();
     }
 }
