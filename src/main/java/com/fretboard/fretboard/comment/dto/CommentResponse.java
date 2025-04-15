b68c7b6f7ac4c9e71d2cb62e5251cae50e79d1e2
siwol025 <cyk99115695@gmail.com>
2025-04-15 21:18:50 +0900

    feat: 게시글 불러올때 해당 게시글의 댓글도 불러오도록 기능 추가

--- src/main/java/com/fretboard/fretboard/comment/dto/CommentResponse.java
+++ src/main/java/com/fretboard/fretboard/comment/dto/CommentResponse.java
@@ -1,8 +1,15 @@
 package com.fretboard.fretboard.comment.dto;
 
+import com.fretboard.fretboard.comment.domain.Comment;
 import java.util.List;
 
 public record CommentResponse(
         List<CommentDetailResponse> contents
 ) {
+    public static CommentResponse createByComments(List<Comment> comments) {
+        List<CommentDetailResponse> commentDetailResponses = comments.stream()
+                .map(CommentDetailResponse::from)
+                .toList();
+        return new CommentResponse(commentDetailResponses);
+    }
 }
--- src/main/java/com/fretboard/fretboard/post/dto/PostDetailResponse.java
+++ src/main/java/com/fretboard/fretboard/post/dto/PostDetailResponse.java
@@ -1,18 +1,23 @@
 package com.fretboard.fretboard.post.dto;
 
+import com.fretboard.fretboard.comment.dto.CommentDetailResponse;
+import com.fretboard.fretboard.comment.dto.CommentResponse;
 import com.fretboard.fretboard.post.domain.Post;
+import java.util.List;
 import lombok.Builder;
 
 public record PostDetailResponse(
         Long id,
         String title,
-        String content
+        String content,
+        CommentResponse commentResponse
 ) {
     @Builder
-    public PostDetailResponse(Long id, String title, String content) {
+    public PostDetailResponse(Long id, String title, String content, CommentResponse commentResponse) {
         this.id = id;
         this.title = title;
         this.content = content;
+        this.commentResponse = commentResponse;
     }
 
     public static PostDetailResponse of(Post post) {
@@ -20,6 +25,7 @@ public record PostDetailResponse(
                 .id(post.getId())
                 .title(post.getTitle())
                 .content(post.getContent())
+                .commentResponse(CommentResponse.createByComments(post.getComments()))
                 .build();
     }
 }
