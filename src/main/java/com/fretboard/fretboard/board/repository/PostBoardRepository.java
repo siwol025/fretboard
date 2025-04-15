20f3ca1e5815aa42fe41805e961e9543a7df468f
siwol025 <cyk99115695@gmail.com>
2025-04-15 18:27:55 +0900

    feat: 게시판별로 전체 게시물 불러오기 기능 구현

--- src/main/java/com/fretboard/fretboard/board/repository/PostBoardRepository.java
+++ src/main/java/com/fretboard/fretboard/board/repository/PostBoardRepository.java
@@ -1,9 +1,11 @@
 package com.fretboard.fretboard.board.repository;
 
 import com.fretboard.fretboard.board.domain.PostBoard;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostBoardRepository extends JpaRepository<PostBoard, Long> {
+    List<PostBoard> findPostBoardsByBoardId(Long boardId);
 }
