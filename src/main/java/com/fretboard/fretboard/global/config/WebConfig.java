60a92d917997e4665a4d2cd13cec20cf9109c01d
siwol025 <cyk99115695@gmail.com>
2025-04-24 23:42:37 +0900

    chore(cors): 프론트 요청 허용을 위한 CORS 설정 추가

--- src/main/java/com/fretboard/fretboard/global/config/WebConfig.java
+++ src/main/java/com/fretboard/fretboard/global/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.fretboard.fretboard.global.auth.resolver.LoginMemberArgumentResolver;
 import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -17,5 +18,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginMemberArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
